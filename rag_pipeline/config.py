4f677a12da6c6824146ed943cc3d250577c3728f
Dinesh Chaudhary <50474936+aimldinesh@users.noreply.github.com>
2026-06-29 09:48:15 +0530

    Clean up blank line in config.py
    Removed unnecessary blank line in config.py

--- rag_pipeline/config.py
+++ rag_pipeline/config.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Config:
     ASTRA_DB_API_ENDPOINT = os.getenv("ASTRA_DB_API_ENDPOINT")
     ASTRA_DB_APPLICATION_TOKEN = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
